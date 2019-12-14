73a629cc359e566429ed5f68933257b6acbf4b91
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-14 16:57:39 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
 # Add  code here!
+
+def prime?(int)
+  
+  range = (2..int - 1).to_a 
+  
+  if int == 2 || int == 3
+    
+    return true
+    
+  elsif int <= 1 
+  
+    return false
+    
+  end
+  
+  range.any? do |divider| if int % divider == 0 
+      return false
+      end
+    end
+  
+  true 
+end 
