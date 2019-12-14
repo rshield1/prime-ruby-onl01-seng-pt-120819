efa9638b75b3e35f2574448d6e9180c3e2ba3baa
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-14 16:08:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,11 @@
 # Add  code here!
+
+
 def prime?(int)
-  
-  
-  
+  how to determine if a number is a prime 
+create a range of numbers then turn into array  
+  so we can iterate over it
+
+
+return bool  
 end
