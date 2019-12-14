954008fdefe8da7e84efa766b6ce568fc45c00d2
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-14 16:20:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,10 @@
 
 
 def prime?(int)
-  how to determine if a number is a prime 
-create a range of numbers then turn into array  
-  so we can iterate over it
+  #how to determine if a number is a prime 
+#create a range of numbers then turn into array  
+  #so we can iterate over it
 
 
-return bool  
+#return bool  
 end
